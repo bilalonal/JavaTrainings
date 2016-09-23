04f1876fa5eb09c2c5fd41399adbb370cfcad365
Bilal onal <alpfaruk71_@hotmail.com>
2016-09-22 23:09:39 -0600

    basic trainings about while loop

---
+++ thenewbostonAndBook/WhileLoop.java
@@ -0,0 +1,21 @@
+package thenewbostonAndBook;
+
+public class WhileLoop {
+	public static void main (String[]args){
+		int counter = 0;
+		
+		while(counter < 10){
+			System.out.println(counter);
+			counter++;
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
