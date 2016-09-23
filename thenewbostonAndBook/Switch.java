6d60c928b4137312fe22085b53abc99c318af051
Bilal onal <alpfaruk71_@hotmail.com>
2016-09-22 23:09:07 -0600

    Basic Tranings about switch

---
+++ thenewbostonAndBook/Switch.java
@@ -0,0 +1,36 @@
+package thenewbostonAndBook;
+
+public class Switch {
+	public static void main(String[]args){
+		int age = 5;
+		
+		switch(age){
+			case 1:
+				System.out.println("noob");
+				break;
+			case 2:
+				System.out.println("beginner");
+				break;
+			case 3:
+				System.out.println("advanced");
+				break;
+			default:
+				System.out.println("expert");
+				break;
+		}
+		
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
