ba96c4b55a0c7b50042433aef6ab79b93e42a23e
Bilal onal <alpfaruk71_@hotmail.com>
2016-09-24 23:31:52 -0600

    trainings(multiple classes)

---
+++ MultipleClassesWork/UMC2.java
@@ -0,0 +1,8 @@
+package MultipleClassesWork;
+
+public class UMC2 {
+public void simpleMessage(){
+	System.out.println("Hello!This is UMC2.");
+	System.out.println("Welcome to our class!");
+}
+}
---
+++ MultipleClassesWork/UsingMultipleClasses.java
@@ -0,0 +1,9 @@
+package MultipleClassesWork;
+
+public class UsingMultipleClasses {
+	public static void main(String[]args){
+		
+		UMC2 umc2Object = new UMC2();
+		umc2Object.simpleMessage();	
+	}
+}
